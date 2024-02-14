c4b2a6c4a906a248e5af228fcd9591a3a81e8b50
CarinWood <carin.wood.85@gmail.com>
2024-02-14 10:24:27 +0100

    worked on menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,19 +1,38 @@
-import { useState } from 'react'
-import './menu.css'
-import { BsCardText } from "react-icons/bs";
+import { useState } from "react";
+import "./menu.css";
+import { BsCardText, BsPencil } from "react-icons/bs";
 
-export const Menu = () => {
-    const [menuAway, setMenuAway] = useState(false)
+export const Menu = ({ setShowFlash, setShowSpell }) => {
+  const [menuAway, setMenuAway] = useState(false);
 
-    const flash = () => {
-        setMenuAway(true)
-    }
+  const flash = () => {
+    setMenuAway(true);
+    setTimeout(() => {
+      setShowFlash(true);
+    }, 100);
+  };
+
+  const spell = () => {
+    setMenuAway(true);
+    setTimeout(() => {
+      setShowSpell(true);
+    }, 100);
+  };
 
   return (
-    <ul className={menuAway ? "menu away": "menu"}>
-        <li onClick={flash}>Flashcards <BsCardText/></li>
-        <li>Skriv orden</li>
-        <li>Para ihop</li>
-    </ul>
-  )
-}
+    <div className={menuAway ? "menu away" : "menu"}>
+      <p className="menu-headline">Hur vill du träna?</p>
+      <ul className="menu-list">
+        <li onClick={flash}>
+          <BsCardText className="icon" />
+          Flashcards
+        </li>
+
+        <li onClick={spell}>
+          <BsPencil className="icon" />
+          Skriv orden
+        </li>
+      </ul>
+    </div>
+  );
+};
--- src/pages/Zero.jsx
+++ src/pages/Zero.jsx
@@ -3,16 +3,18 @@ import Carousel from "../components/carousel/Carousel";
 import { ZeroList } from "../data/Zero";
 import "../styles/zero.css";
 import { useState } from "react";
-import { Spell } from '../components/spell/Spell'
+import { Spell } from "../components/spell/Spell";
+import { Menu } from "../components/menu/Menu";
 
 export const Zero = () => {
   const [showFlash, setShowFlash] = useState(false);
-  const [showSpell, setShowSpell] = useState(true);
+  const [showSpell, setShowSpell] = useState(false);
   return (
     <div className="container">
       <Back />
+      <Menu setShowFlash={setShowFlash} setShowSpell={setShowSpell}/>
       {showFlash && <Carousel flashcardList={ZeroList} />}
-      {showSpell && <Spell wordList={ZeroList}/>}
+      {showSpell && <Spell wordList={ZeroList} />}
     </div>
   );
 };
