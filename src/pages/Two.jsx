b6b4b50a145cbe71f1fbbfc4f2c740893d9a3916
CarinWood <carin.wood.85@gmail.com>
2024-02-16 13:46:49 +0100

    update match game for second chapter

--- src/pages/Two.jsx
+++ src/pages/Two.jsx
@@ -5,17 +5,21 @@ import { SecondList } from "../data/Second";
 import { useState } from "react";
 import { Menu } from "../components/menu/Menu";
 import { Spell } from "../components/spell/Spell";
+import { secondMatchList } from "../data/Second";
+import { Match } from "../components/match/Match";
 
 export const Two = () => {
   const [showFlash, setShowFlash] = useState(false);
   const [showSpell, setShowSpell] = useState(false);
+  const [showMatch, setShowMatch] = useState(false);
 
   return (
     <div className="container">
     <Back />
-    <Menu setShowFlash={setShowFlash} setShowSpell={setShowSpell}/>
+    <Menu setShowFlash={setShowFlash} setShowSpell={setShowSpell} setShowMatch={setShowMatch}/>
     {showFlash && <Carousel flashcardList={SecondList} />}
     {showSpell && <Spell wordList={SecondList} />}
+    {showMatch && <Match wordList={secondMatchList} />}
   </div>
   );
 };
