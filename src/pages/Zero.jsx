af6d6ce2a46136ee195c134f2eccfa0a345fba29
CarinWood <carin.wood.85@gmail.com>
2024-02-16 13:18:22 +0100

    match for zero fixed

--- src/pages/Zero.jsx
+++ src/pages/Zero.jsx
@@ -5,10 +5,13 @@ import "../styles/zero.css";
 import { useState } from "react";
 import { Spell } from "../components/spell/Spell";
 import { Menu } from "../components/menu/Menu";
+import { Match } from "../components/match/Match";
+import { zeroMatchList } from "../data/Zero"
 
 export const Zero = () => {
   const [showFlash, setShowFlash] = useState(false);
   const [showSpell, setShowSpell] = useState(false);
+  const [showMatch, setShowMatch] = useState(false);
 
 
   return (
@@ -17,9 +20,11 @@ export const Zero = () => {
       <Menu
         setShowFlash={setShowFlash}
         setShowSpell={setShowSpell}
+        setShowMatch={setShowMatch}
       />
       {showFlash && <Carousel flashcardList={ZeroList} />}
       {showSpell && <Spell wordList={ZeroList} />}
+      {showMatch && <Match wordList={zeroMatchList} />}
     </div>
   );
 };
