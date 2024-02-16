df79f148e1ad2d2a34452127547f3857245b72e8
CarinWood <carin.wood.85@gmail.com>
2024-02-16 13:39:15 +0100

    fixed match game for chapter one

--- src/pages/One.jsx
+++ src/pages/One.jsx
@@ -4,18 +4,23 @@ import Carousel from '../components/carousel/Carousel'
 import { FirstList } from '../data/First'
 import { Menu } from '../components/menu/Menu'
 import { Spell } from '../components/spell/Spell'
+import { firstMatchList } from '../data/First'
+import { Match } from '../components/match/Match'
 
 export const One = () => {
   const [showFlash, setShowFlash] = useState(false);
   const [showSpell, setShowSpell] = useState(false);
+  const [showMatch, setShowMatch] = useState(false);
 
 
   return (
     <div className="container">
     <Back />
-    <Menu setShowFlash={setShowFlash} setShowSpell={setShowSpell}/>
+    <Menu setShowFlash={setShowFlash} setShowSpell={setShowSpell} setShowMatch={setShowMatch}/>
     {showFlash && <Carousel flashcardList={FirstList} />}
     {showSpell && <Spell wordList={FirstList} />}
+    {showMatch && <Match wordList={firstMatchList} />}
+
   </div>
   )
 }
