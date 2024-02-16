4c43d822baf4f75ccd0645463fd82cd223f345ca
CarinWood <carin.wood.85@gmail.com>
2024-02-16 18:42:57 +0100

    update

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -7,34 +7,37 @@ const Overview = () => {
   return (
     <div className="overview">
       <div className="chapters">
-          <ul>
-            <Link to="/kapitel_0" className="list-item">
-              Unité 0
-            </Link>
-            <Link to="/kapitel_1" className="list-item">
-              Unité 1
-            </Link>
-            <Link to="/kapitel_2" className="list-item">
-              Unité 2
-            </Link>
-            <Link to="/kapitel_3" className="list-item">
-              Unité 3
-            </Link>
-          </ul>
+        <ul>
+          <Link to="/kapitel_0" className="list-item">
+            Unité 0
+          </Link>
+          <Link to="/kapitel_1" className="list-item">
+            Unité 1
+          </Link>
+          <Link to="/kapitel_2" className="list-item">
+            Unité 2
+          </Link>
+          <Link to="/kapitel_3" className="list-item">
+            Unité 3
+          </Link>
+          <Link className="list-item">Unité 4</Link>
+          <Link className="list-item">Unité 5</Link>
+        </ul>
 
-          <ul>
-            <Link className="list-item">Unité 4</Link>
-            <Link className="list-item">Unité 5</Link>
-            <Link className="list-item">Unité 6</Link>
-            <Link className="list-item">Unité 7</Link>
-          </ul>
+        <ul>
+          <Link className="list-item">Unité 6</Link>
+          <Link className="list-item">Unité 7</Link>
+          <Link className="list-item">Unité 8</Link>
+          <Link className="list-item">Unité 9</Link>
+          <Link className="list-item">Unité 10</Link>
+        </ul>
       </div>
       <ul className="specials">
         <Link to="/sentences" className="list-item spec">
           <BsPencil className="overview-icon" />
           bygg meningar
         </Link>
-    {/*     <Link to="/challenge" className="list-item spec">
+        {/*     <Link to="/challenge" className="list-item spec">
           <FaFlagCheckered className="overview-icon" />
           Utmaningen
         </Link> */}
