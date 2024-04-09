2b10068ef5688cddd4519315c6adac5ef883aa79
CarinWood <carin.wood.85@gmail.com>
2024-04-09 18:57:46 +0200

    update

--- src/components/quizAlternative/QuizAlternative.jsx
+++ src/components/quizAlternative/QuizAlternative.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './quizAlternative.css'
+import success from '../../assets/audio/success.mp3'
 
 export const QuizAlternative = ({word, wordArray}) => {
     const [redBorder, setRedBorder] = useState(false)
+    const [greenBorder, setGreenBorder] = useState(false)
 
     const clickOnCard = () => {
         if(word === wordArray[0].sv) {
             console.log("right answer!")
+            const audio = new Audio(success);
+            audio.play();
+            setGreenBorder(true)
         } else {
             console.log("wrong answer!")
             setRedBorder(true)
@@ -16,7 +21,7 @@ export const QuizAlternative = ({word, wordArray}) => {
 
 
   return (
-    <div className={redBorder ? "quiz-card red-border" : "quiz-card"} onClick={clickOnCard}>
+    <div className={redBorder ? "quiz-card red-border": greenBorder ? "quiz-card green-border" : "quiz-card"} onClick={clickOnCard}>
         <p>{word}</p>
     </div>
   )
