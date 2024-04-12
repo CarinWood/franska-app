a4d151fd6331afab9bab75728245ed425cbf548a
CarinWood <carin.wood.85@gmail.com>
2024-04-12 09:29:12 +0200

    added click game to chapter zero

--- src/pages/Zero.jsx
+++ src/pages/Zero.jsx
@@ -7,11 +7,13 @@ import { Spell } from "../components/spell/Spell";
 import { Menu } from "../components/menu/Menu";
 import { Match } from "../components/match/Match";
 import { zeroMatchList } from "../data/Zero"
+import Click from "../components/click/Click";
 
 export const Zero = () => {
   const [showFlash, setShowFlash] = useState(false);
   const [showSpell, setShowSpell] = useState(false);
   const [showMatch, setShowMatch] = useState(false);
+  const [showClick, setShowClick] = useState(false);
 
 
   return (
@@ -21,10 +23,12 @@ export const Zero = () => {
         setShowFlash={setShowFlash}
         setShowSpell={setShowSpell}
         setShowMatch={setShowMatch}
+        setShowClick={setShowClick}
       />
       {showFlash && <Carousel flashcardList={ZeroList} />}
       {showSpell && <Spell wordList={ZeroList} />}
       {showMatch && <Match wordList={zeroMatchList} />}
+      {showClick && <Click wordList={zeroMatchList} />}
     </div>
   );
 };
