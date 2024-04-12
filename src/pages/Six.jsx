25877cefb3470aae630c5ea37d4c6f58f056ede4
CarinWood <carin.wood.85@gmail.com>
2024-04-12 09:55:22 +0200

    fix with chapter six and added a sixthMatchList

--- src/pages/Six.jsx
+++ src/pages/Six.jsx
@@ -4,12 +4,14 @@ import { Menu } from '../components/menu/Menu';
 import Carousel from '../components/carousel/Carousel';
 import { Spell } from '../components/spell/Spell';
 import { Match } from '../components/match/Match';
-import { SixthList } from '../data/Sixth';
+import { SixthList, SixthMatchList } from '../data/Sixth';
+import Click from '../components/click/Click';
 
 const Six = () => {
     const [showFlash, setShowFlash] = useState(false);
     const [showSpell, setShowSpell] = useState(false);
     const [showMatch, setShowMatch] = useState(false);
+    const [showClick, setShowClick] = useState(false);
   return (
     <div className="container">
       <Back />
@@ -17,10 +19,12 @@ const Six = () => {
         setShowFlash={setShowFlash}
         setShowSpell={setShowSpell}
         setShowMatch={setShowMatch}
+        setShowClick={setShowClick}
       />
       {showFlash && <Carousel flashcardList={SixthList} />}
       {showSpell && <Spell wordList={SixthList} />}
-      {showMatch && <Match wordList={SixthList} />}
+      {showMatch && <Match wordList={SixthMatchList} />}
+      {showClick && <Click wordList={SixthMatchList} />}
     </div>
   )
 }
