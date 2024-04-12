3b4345afb2ddec9aa2eb52dea6a916c5ebf39c7d
CarinWood <carin.wood.85@gmail.com>
2024-04-12 09:37:15 +0200

    added click game to chapter four

--- src/pages/Four.jsx
+++ src/pages/Four.jsx
@@ -5,11 +5,13 @@ import Carousel from "../components/carousel/Carousel";
 import { Spell } from "../components/spell/Spell";
 import { Match } from "../components/match/Match";
 import { FourthList, FourthMatchList } from "../data/fourth";
+import Click from "../components/click/Click";
 
 export const Four = () => {
   const [showFlash, setShowFlash] = useState(false);
   const [showSpell, setShowSpell] = useState(false);
   const [showMatch, setShowMatch] = useState(false);
+  const [showClick, setShowClick] = useState(false);
   return (
     <div className="container">
       <Back />
@@ -17,10 +19,12 @@ export const Four = () => {
         setShowFlash={setShowFlash}
         setShowSpell={setShowSpell}
         setShowMatch={setShowMatch}
+        setShowClick={setShowClick}
       />
       {showFlash && <Carousel flashcardList={FourthList} />}
       {showSpell && <Spell wordList={FourthList} />}
       {showMatch && <Match wordList={FourthMatchList} />}
+      {showClick && <Click wordList={FourthMatchList} />}
     </div>
   );
 };
