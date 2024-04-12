1f828de9edae71df955aa27b37ed46ebf31af5ca
CarinWood <carin.wood.85@gmail.com>
2024-04-12 09:39:12 +0200

    added click game to chapter five

--- src/pages/Five.jsx
+++ src/pages/Five.jsx
@@ -5,11 +5,13 @@ import { Menu } from '../components/menu/Menu';
 import Carousel from '../components/carousel/Carousel';
 import { Spell } from '../components/spell/Spell';
 import { Match } from '../components/match/Match';
+import Click from '../components/click/Click';
 
 const Five = () => {
     const [showFlash, setShowFlash] = useState(false);
     const [showSpell, setShowSpell] = useState(false);
     const [showMatch, setShowMatch] = useState(false);
+    const [showClick, setShowClick] = useState(false);
   return (
     <div className="container">
       <Back />
@@ -17,10 +19,12 @@ const Five = () => {
         setShowFlash={setShowFlash}
         setShowSpell={setShowSpell}
         setShowMatch={setShowMatch}
+        setShowClick={setShowClick}
       />
       {showFlash && <Carousel flashcardList={fifthList} />}
       {showSpell && <Spell wordList={fifthList} />}
       {showMatch && <Match wordList={fifthList} />}
+      {showClick && <Click wordList={fifthList} />}
     </div>
   )
 }
